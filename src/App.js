3e14e76cb6f3afc211c85ae9bba10c0831add507
Neha <nehabhat2209@gmail.com>
2025-06-06 23:32:04 +0530

    added basic layout with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import SessionsList from './SessionsList';
+import ChatWindow from './ChatWindow';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Container fluid>
+        <Row>
+        <Col sm={3}>
+          <SessionsList />
+        </Col>
+        <Col>
+          <Row>
+            <Header />
+          </Row>
+          <Row>
+            <ChatWindow />
+          </Row>
+        </Col>
+      </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/DefaultChatWindow.js
@@ -0,0 +1,19 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+
+const DefaultChatWindow = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <h3>What can I help you with?</h3>
+                </Row>
+                <Row>
+                    
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default DefaultChatWindow;
