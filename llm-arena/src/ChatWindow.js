ba3ead504194e210db77d7f150a53c218b16d61b
Neha <nehabhat2209@gmail.com>
2025-06-13 18:10:12 +0530

    Initial commit with llm-arena and llm-arena-backend

---
+++ llm-arena/src/ChatWindow.js
@@ -0,0 +1,11 @@
+import DefaultChatWindow from "./DefaultChatWindow"
+
+const ChatWindow = () => {
+    return (
+        <div className="chat-container">
+            <DefaultChatWindow />
+        </div>
+    )
+}
+
+export default ChatWindow
---
+++ llm-arena/src/DefaultChatWindow.js
@@ -0,0 +1,27 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+
+const DefaultChatWindow = () => {
+    const displayMessage = [];
+    const message = ['W', 'h', 'a', 't', 'c', 'a', 'n', 'I', 'h', 'e', 'l', 'p', 'y', 'o', 'u', 'w', 'i', 't', 'h', '?']
+    // setInterval(() => {
+    //     // console.log("push")
+    //     displayMessage.push(...message)
+    //     console.log(displayMessage)
+    //     // message.map(character => displayMessage.push(character))
+    // }, 500)
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <h3>What can I help you with?</h3>
+                </Row>
+                <Row>
+                    
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default DefaultChatWindow;
